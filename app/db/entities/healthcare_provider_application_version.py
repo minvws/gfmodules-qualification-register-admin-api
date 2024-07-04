a95bd1f1db5eebb8fd95e93eca2089ac82c26862
Gerbrand Bosch <gerbrandb@gmail.com>
2024-07-04 13:05:43 +0200

    Remove cascade from application_version

--- app/db/entities/healthcare_provider_application_version.py
+++ app/db/entities/healthcare_provider_application_version.py
@@ -44,7 +44,7 @@ class HealthcareProviderApplicationVersion(Base):
     )
 
     healthcare_provider: Mapped["healthcare_provider.HealthcareProvider"] = (
-        relationship(back_populates="application_versions")
+        relationship(back_populates="application_versions", lazy="selectin")
     )
     application_version: Mapped["application_version.ApplicationVersion"] = (
         relationship(back_populates="healthcare_providers", lazy="selectin")
