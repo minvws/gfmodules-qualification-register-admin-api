a60785e89eb0c3b9c2a0a1f204b0da4927edd6e3
Herman van Rink <rink@initfour.nl>
2024-07-09 14:31:49 +0200

    cleanup

--- sql/003-user-split.sql
+++ sql/003-user-split.sql
@@ -1,4 +1,5 @@
---- Create web user qualification
+-- Split users
+REVOKE ALL ON TABLE deploy_releases FROM qualification
 DROP ROLE qualification;
 
 CREATE ROLE qualification_admin;
@@ -11,19 +12,19 @@ GRANT SELECT ON deploy_releases TO qualification_admin;
 GRANT SELECT ON deploy_releases TO qualification_api;
 
 
-GRANT SELECT,UPDATE,DELETE ON vendors TO qualification_admin;
-GRANT SELECT,UPDATE,DELETE ON protocols TO qualification_admin;
-GRANT SELECT,UPDATE,DELETE ON protocol_versions TO qualification_admin;
-GRANT SELECT,UPDATE,DELETE ON roles TO qualification_admin;
-GRANT SELECT,UPDATE,DELETE ON system_types TO qualification_admin;
-GRANT SELECT,UPDATE,DELETE ON applications TO qualification_admin;
-GRANT SELECT,UPDATE,DELETE ON application_versions TO qualification_admin;
-GRANT SELECT,UPDATE,DELETE ON healthcare_providers TO qualification_admin;
-GRANT SELECT,UPDATE,DELETE ON applications_roles TO qualification_admin;
-GRANT SELECT,UPDATE,DELETE ON applications_types TO qualification_admin;
-GRANT SELECT,UPDATE,DELETE ON healthcare_providers_application_versions TO qualification_admin;
-GRANT SELECT,UPDATE,DELETE ON healthcare_providers_qualifications TO qualification_admin;
-GRANT SELECT,UPDATE,DELETE ON protocol_application_qualifications TO qualification_admin;
+GRANT SELECT,INSERT,UPDATE,DELETE ON vendors TO qualification_admin;
+GRANT SELECT,INSERT,UPDATE,DELETE ON protocols TO qualification_admin;
+GRANT SELECT,INSERT,UPDATE,DELETE ON protocol_versions TO qualification_admin;
+GRANT SELECT,INSERT,UPDATE,DELETE ON roles TO qualification_admin;
+GRANT SELECT,INSERT,UPDATE,DELETE ON system_types TO qualification_admin;
+GRANT SELECT,INSERT,UPDATE,DELETE ON applications TO qualification_admin;
+GRANT SELECT,INSERT,UPDATE,DELETE ON application_versions TO qualification_admin;
+GRANT SELECT,INSERT,UPDATE,DELETE ON healthcare_providers TO qualification_admin;
+GRANT SELECT,INSERT,UPDATE,DELETE ON applications_roles TO qualification_admin;
+GRANT SELECT,INSERT,UPDATE,DELETE ON applications_types TO qualification_admin;
+GRANT SELECT,INSERT,UPDATE,DELETE ON healthcare_providers_application_versions TO qualification_admin;
+GRANT SELECT,INSERT,UPDATE,DELETE ON healthcare_providers_qualifications TO qualification_admin;
+GRANT SELECT,INSERT,UPDATE,DELETE ON protocol_application_qualifications TO qualification_admin;
 ;
 GRANT SELECT ON vendors TO qualification_api;
 GRANT SELECT ON protocols TO qualification_api;
