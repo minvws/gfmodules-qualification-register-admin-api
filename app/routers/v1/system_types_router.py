38e737e933837296952b6e9bf41570bc0d68f773
Rick Lambrechts <rick@rl-webdiensten.nl>
2024-07-11 22:30:45 +0200

    Use kebab casing for API endpoints

--- app/routers/v1/system_types_router.py
+++ app/routers/v1/system_types_router.py
@@ -7,7 +7,7 @@ from app.schemas.system_type.mapper import map_system_type_entity_to_dto
 from app.schemas.system_type.schema import SystemTypeCreateDTO, SystemTypeDTO
 from app.db.services.system_type_service import SystemTypeService
 
-router = APIRouter(prefix="/system_types", tags=["System Types"])
+router = APIRouter(prefix="/system-types", tags=["System Types"])
 
 
 @router.get("")
