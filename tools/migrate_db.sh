151cfea072e7459d997ba45622cf43c8e8aeee88
Rick Lambrechts <rick@rl-webdiensten.nl>
2024-08-30 14:36:14 +0200

    chore: cd to project path in migrate db script

--- tools/migrate_db.sh
+++ tools/migrate_db.sh
@@ -4,6 +4,7 @@ set -e
 
 SCRIPT_PATH=`realpath $0`
 DIR_PATH=`dirname $SCRIPT_PATH`
+PROJECT_PATH="$DIR_PATH/.."
 
 DB_HOST=${1:-localhost}
 DB_USER=${2:-postgres}
@@ -17,6 +18,9 @@ YELLOW="\033[33m"
 BLUE="\033[34m"
 NC="\033[0m"
 
+# Execute commands from the project directory
+cd $PROJECT_PATH
+
 echo -e "${GREEN}👀 Checking migrations for ${BLUE}$DB_NAME${GREEN} on ${BLUE}$DB_HOST${NC}"
 
 # check if the migration table exists
@@ -29,7 +33,7 @@ if
     echo "CREATE TABLE migrations (id serial PRIMARY KEY, name VARCHAR(255) NOT NULL, created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);" | psql -h $DB_HOST -U $DB_USER -d $DB_NAME -q -o /dev/null
 fi
 
-for file_path in $DIR_PATH/../sql/*.sql; do
+for file_path in sql/*.sql; do
     file_name=$(basename $file_path)
 
     # Check each SQL file to see if it's already in the migrations table
