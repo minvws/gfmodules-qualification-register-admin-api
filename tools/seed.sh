75b2155544886f1c613d57326a02e9f5179c17e7
Rick Lambrechts <rick@rl-webdiensten.nl>
2024-08-30 09:57:26 +0200

    refactor: use relative seed folder

--- tools/seed.sh
+++ tools/seed.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+SCRIPT_PATH=`realpath $0`
+DIR_PATH=`dirname $SCRIPT_PATH`
+PROJECT_PATH="$DIR_PATH/.."
+
 DB_HOST=${1:-localhost}
 DB_USER=${2:-postgres}
 DB_PASS=${3:-postgres}
@@ -16,6 +20,9 @@ NC="\033[0m"
 
 shopt -s nullglob
 
+# Execute commands from the project directory
+cd $PROJECT_PATH
+
 echo -e "${GREEN}👀 Checking seed migrations for ${BLUE}$DB_NAME${GREEN} on ${BLUE}$DB_HOST${NC}"
 
 # check if the seed_migrations table exists
@@ -28,25 +35,27 @@ if
     echo "CREATE TABLE seed_migrations (id serial PRIMARY KEY, name VARCHAR(255) NOT NULL, created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);" | psql -h $DB_HOST -U $DB_USER -d $DB_NAME -q -o /dev/null
 fi
 
-for file in seeds/*.sql; do
+for file_path in seeds/*.sql; do
+    file_name=$(basename $file_path)
     # Check each SQL file to see if it's already in the migrations table
-    if psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c "SELECT name FROM seed_migrations WHERE name = '$file';" | grep -q $file; then
+    if psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c "SELECT name FROM seed_migrations WHERE name = '$file_name';" | grep -q $file_name; then
         echo -e "${YELLOW}⏩ File $file is already in the seed_migrations table. Skipping.${NC}"
     else
-        echo -e "${GREEN}▶️ Running seed_migrations $file${NC}"
-        psql -h $DB_HOST -U $DB_USER -d $DB_NAME -f $file -q -o /dev/null
-        echo "INSERT INTO seed_migrations (name) VALUES ('$file');" | psql -h $DB_HOST -U $DB_USER -d $DB_NAME -o /dev/null
+        echo -e "${GREEN}▶️ Running seed_migrations $file_name${NC}"
+        psql -h $DB_HOST -U $DB_USER -d $DB_NAME -f $file_path --single-transaction -v ON_ERROR_STOP=1 -q -o /dev/null
+        echo "INSERT INTO seed_migrations (name) VALUES ('$file_name');" | psql -h $DB_HOST -U $DB_USER -d $DB_NAME -o /dev/null
     fi
 done
 
-for file in seeds/*.py; do
+for file_path in seeds/*.py; do
+    file_name=$(basename $file_path)
     # Check each SQL file to see if it's already in the migrations table
-    if psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c "SELECT name FROM seed_migrations WHERE name = '$file';" | grep -q $file; then
+    if psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c "SELECT name FROM seed_migrations WHERE name = '$file_name';" | grep -q $file_name; then
         echo -e "${YELLOW}⏩ File $file is already in the seed_migrations table. Skipping.${NC}"
     else
-        echo -e "${GREEN}▶️ Running seed_migrations $file${NC}"
-        python $file
-        echo "INSERT INTO seed_migrations (name) VALUES ('$file');" | psql -h $DB_HOST -U $DB_USER -d $DB_NAME -o /dev/null
+        echo -e "${GREEN}▶️ Running seed_migrations $file_name${NC}"
+        python $file_path
+        echo "INSERT INTO seed_migrations (name) VALUES ('$file_name');" | psql -h $DB_HOST -U $DB_USER -d $DB_NAME -o /dev/null
     fi
 done
 
