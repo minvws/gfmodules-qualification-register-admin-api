fe63ae17a14ceaaedb660b23a76a03b7d603135a
Thomas Bosch <bosch-thomas@protonmail.com>
2024-09-13 11:26:19 +0200

    Use docker compose health check instead

--- docker/init.sh
+++ docker/init.sh
@@ -2,23 +2,12 @@
 
 set -e
 
-DB_STARTUP_WAIT=1
-
 DB_HOST=${1}
 DB_USER=${2:-postgres}
 DB_PASS=${3:-postgres}
 DB_NAME=${4:-postgres}
 
 export PGPASSWORD=$DB_PASS
-
-set +e
-psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c "SELECT 1" >/dev/null
-while [ $? -ne 0 ]; do
-  echo "Waiting for db container to finish startup..."
-  sleep $DB_STARTUP_WAIT
-  psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c "SELECT 1" >/dev/null
-done
-set -e
 
 echo "Migrating"
 tools/./migrate_db.sh $DB_HOST $DB_USER $DB_PASS $DB_NAME
